3ab62821e3ea33864e4eaec177230d2ca0625b0d
Bill Chan MBA <billypchan@gmail.com>
2017-09-25 16:02:37 +0200

    watch OS support

---
+++ FMDB watchOS/FMDB_watchOS.h
@@ -0,0 +1,18 @@
+//
+//  FMDB_watchOS.h
+//  FMDB watchOS
+//
+//  Created by chan bill on 25/9/2017.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for FMDB_watchOS.
+FOUNDATION_EXPORT double FMDB_watchOSVersionNumber;
+
+//! Project version string for FMDB_watchOS.
+FOUNDATION_EXPORT const unsigned char FMDB_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMDB_watchOS/PublicHeader.h>
+
+
